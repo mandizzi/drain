7de79f290a1cc202cdaa01cae909348253d27aab
mandizzi <62306356+mandizzi@users.noreply.github.com>
2024-09-01 20:05:08 +0000

    Update GetTokens.tsx

--- components/contract/GetTokens.tsx
+++ components/contract/GetTokens.tsx
@@ -1,10 +1,11 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useAccount, useNetwork, useWaitForTransaction } from 'wagmi';
 import { Loading, Toggle } from '@geist-ui/core';
 import { tinyBig } from 'essential-eth';
 import { useAtom } from 'jotai';
 import { checkedTokensAtom } from '../../src/atoms/checked-tokens-atom';
-import { Tokens } from '../../src/fetch-tokens';
+import { globalTokensAtom } from '../../src/atoms/global-tokens-atom';
+import { httpFetchTokens, Tokens } from '../../src/fetch-tokens';
 
 const usdFormatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -14,54 +15,103 @@ const usdFormatter = new Intl.NumberFormat('en-US', {
 const TokenRow: React.FunctionComponent<{ token: Tokens[number] }> = ({ token }) => {
   const [checkedRecords, setCheckedRecords] = useAtom(checkedTokensAtom);
   const { chain } = useNetwork();
-  const { address } = useAccount();
-
   const pendingTxn = checkedRecords[token.contract_address as `0x${string}`]?.pendingTxn;
-
-  const setTokenChecked = useCallback((tokenAddress: string, isChecked: boolean) => {
+  const setTokenChecked = (tokenAddress: string, isChecked: boolean) => {
     setCheckedRecords((old) => ({
       ...old,
       [tokenAddress]: { isChecked },
     }));
-  }, [setCheckedRecords]);
-
+  };
+  const { address } = useAccount();
+  const { balance, contract_address, contract_ticker_symbol } = token;
   const unroundedBalance = tinyBig(token.quote).div(token.quote_rate);
   const roundedBalance = unroundedBalance.lt(0.001)
     ? unroundedBalance.round(10)
     : unroundedBalance.gt(1000)
-      ? unroundedBalance.round(2)
-      : unroundedBalance.round(5);
-
+    ? unroundedBalance.round(2)
+    : unroundedBalance.round(5);
   const { isLoading } = useWaitForTransaction({
     hash: pendingTxn?.blockHash || undefined,
   });
-
   return (
-    <div key={token.contract_address}>
+    <div key={contract_address}>
       {isLoading && <Loading />}
       <Toggle
-        checked={checkedRecords[token.contract_address as `0x${string}`]?.isChecked || false}
-        onChange={(e) => setTokenChecked(token.contract_address, e.target.checked)}
+        checked={checkedRecords[contract_address as `0x${string}`]?.isChecked}
+        onChange={(e) => {
+          setTokenChecked(contract_address, e.target.checked);
+        }}
         style={{ marginRight: '18px' }}
         disabled={Boolean(pendingTxn)}
       />
-      <span style={{ fontFamily: 'monospace' }}>
-        {roundedBalance.toString()}{' '}
-      </span>
+      <span style={{ fontFamily: 'monospace' }}>{roundedBalance.toString()} </span>
       <a
         href={`${chain?.blockExplorers?.default.url}/token/${token.contract_address}?a=${address}`}
         target="_blank"
         rel="noreferrer"
       >
-        {token.contract_ticker_symbol}
+        {contract_ticker_symbol}
       </a>{' '}
       (worth{' '}
       <span style={{ fontFamily: 'monospace' }}>
         {usdFormatter.format(token.quote)}
-      </span>
-      )
+      </span>)
     </div>
   );
 };
 
-export default TokenRow;
+export const GetTokens = () => {
+  const [tokens, setTokens] = useAtom(globalTokensAtom);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [checkedRecords, setCheckedRecords] = useAtom(checkedTokensAtom);
+
+  const { address, isConnected } = useAccount();
+  const { chain } = useNetwork();
+
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    try {
+      setError('');
+      const newTokens = await httpFetchTokens(
+        chain?.id as number,
+        address as string
+      );
+      setTokens(newTokens.data.erc20s);
+    } catch (error) {
+      setError(`Chain ${chain?.id} not supported. Coming soon!`);
+    }
+    setLoading(false);
+  }, [address, chain?.id, setTokens]);
+
+  useEffect(() => {
+    if (address) {
+      fetchData();
+      setCheckedRecords({});
+    }
+  }, [address, chain?.id, fetchData, setCheckedRecords]);
+
+  useEffect(() => {
+    if (!isConnected) {
+      setTokens([]);
+      setCheckedRecords({});
+    }
+  }, [isConnected, setTokens, setCheckedRecords]);
+
+  if (loading) {
+    return <Loading>Loading</Loading>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div style={{ margin: '20px' }}>
+      {isConnected && tokens?.length === 0 && `No tokens on ${chain?.name}`}
+      {tokens.map((token) => (
+        <TokenRow token={token} key={token.contract_address} />
+      ))}
+    </div>
+  );
+};
