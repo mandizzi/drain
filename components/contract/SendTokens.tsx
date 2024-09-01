b281d384caea98eefec3e62fc413d8479d5a422b
mandizzi <62306356+mandizzi@users.noreply.github.com>
2024-09-01 21:07:58 +0000

    Update SendTokens.tsx

--- components/contract/SendTokens.tsx
+++ components/contract/SendTokens.tsx
@@ -95,25 +95,27 @@ export const SendTokens = () => {
   return (
     <div style={{ margin: '20px' }}>
       <form>
-        Destination Address:
-        <Input
-          required
-          value={destinationAddress}
-          placeholder="vitalik.eth"
-          onChange={(e) => setDestinationAddress(e.target.value)}
-          type={
-            addressAppearsValid
-              ? 'success'
-              : destinationAddress.length > 0
-              ? 'warning'
-              : 'default'
-          }
-          width="100%"
-          style={{
-            marginLeft: '10px',
-            marginRight: '10px',
-          }}
-        />
+        <label>
+          Destination Address:
+          <Input
+            required
+            value={destinationAddress}
+            placeholder="vitalik.eth"
+            onChange={(e) => setDestinationAddress(e.target.value)}
+            type={
+              addressAppearsValid
+                ? 'success'
+                : destinationAddress.length > 0
+                ? 'warning'
+                : 'default'
+            }
+            width="100%"
+            style={{
+              marginLeft: '10px',
+              marginRight: '10px',
+            }}
+          />
+        </label>
         <Button
           type="secondary"
           onClick={sendAllCheckedTokens}
